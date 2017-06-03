104dff8fb35667d60b37a7c7cc9f80ad32560e92
sandesh sardar <sardar.sandesh@gmail.com>
2017-06-03 15:22:02 +0530

    Alamofire first implementation with get and post method

--- APIwithAlamofire/ViewController.swift
+++ APIwithAlamofire/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
+    
+    //---- variable --(start)--
+    var articleList : NSArray?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    self.newFeedGETrequest()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    //---- api call for newsFeed --
+    func newFeedGETrequest(){
+        
+        //---- GET request ---------
+        
+        let url: String = "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=f94a0d53cd3647c985944689b055025d"
+        
+        Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default)
+            .responseJSON { response in
+                switch response.result{
+                case .success( _):
+                    let json = response.result.value as! NSDictionary
+                    print("serviceForNewsFeed response \(json)")
+                    
+                case .failure(let error):
+                    print("error \(error)")
+                }
+        }
+    }
 
+    
+    func newFeedPOSTrequest(){
+        
+        //---- POST request ---------
+        
+        let parameter =  ["timezone": "Asia/Kolkata", "username": "xyz@gmail.com", "password": "abcd1234"]
+        
+        let url: String = "your post api url"
+        
+        Alamofire.request(url, method: .post, parameters: parameter, encoding: JSONEncoding.default)
+            .responseJSON { response in
+                switch response.result{
+                case .success( _):
+                    let json = response.result.value as! NSDictionary
+                    print("json \(json)")
+                    
+                case .failure(let error):
+                    print("error \(error)")
+                }
+        }
+    }
+
+    
 }
 
