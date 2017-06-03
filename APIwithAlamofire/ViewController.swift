a5238584a8ae74ceed34f19920b1652b74bf7b8d
sandesh sardar <sardar.sandesh@gmail.com>
2017-06-03 12:13:59 +0530

    Initial Commit

---
+++ APIwithAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APIwithAlamofire
+//
+//  Created by sandesh sardar on 03/06/17.
+//  Copyright © 2017 sandesh sardar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
